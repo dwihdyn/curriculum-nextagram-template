ab1fb0c4fc45586040ce23064ae4cfbe440b5164
Dwi <jamaltan31@gmail.com>
2020-01-03 15:19:57 +0800

    updating

--- instagram_api/blueprints/users/views.py
+++ instagram_api/blueprints/users/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify
 from models.user import UserCredential
+from models.image import ImageFeed
 
 # # JWT : JSON Web Token handle login part of our UserCredential database | jwt_required same as login_required
 # from flask_jwt import jwt_required
@@ -17,26 +18,28 @@ def index():
 
 # usually we render HTML in flask using `return render_template('home.html')`
 # in API, we return JSON. how ? by using jsonify to convert from our python code to JSON
-@users_api_blueprint.route('/<username>', methods=['GET'])
-def show(username):
-    user = UserCredential.get_or_none(UserCredential.username == username)
+@users_api_blueprint.route('/show', methods=['GET'])
+def show():
+    all_users = UserCredential.select().prefetch(ImageFeed)
+    # resp = [
+    #     {
+    #         "id": i.id,
+    #         "username": i.username,
+    #         "profileImage": i.profile_image_url
+    #     }   
+    #     for i in all_users
+    # ]
 
-    if not user:
-        # return result in JSON format, converted using jsonify
-        resp = {
-            'message': 'No user found with that given username',
-            'status': 404
-        }
-        return jsonify(resp)
+    resp = []
+    for i in all_users:
+        resp.append(        {
+            "id": i.id,
+            "username": i.username,
+            "profileImage": i.profile_image_url
+        }) 
 
-    resp = {
-        'message': 'User found!, showing the user info',
-        'status': 200,
-        'data': [{
-            'id': user.id,
-            'username': user.username,
-            'email': user.email,
-            'profileImg': user.profile_image_url
-        }]
-    }
+
+
+
+
     return jsonify(resp)
